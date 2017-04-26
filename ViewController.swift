4565fc9177b1bd37073fa6132bbf812124f1fbd8
Khalif Moore-Stevenson <kmoorestevenson@gmail.com>
2017-04-26 11:07:42 -0700

    Initial Prototype

---
+++ ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Cmpe137finalProject
+//
+//  Created by Computer on 4/25/17.
+//  Copyright © 2017 Computer. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
