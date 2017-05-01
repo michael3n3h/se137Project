593d654f438d0d9f2d60949a9eca8c591c140e46
Computer <kmoorestevenson@KMS-2.local>
2017-04-30 21:43:46 -0700

    re-push 5

--- Cmpe137finalProject/ViewController.swift
+++ Cmpe137finalProject/ViewController.swift
@@ -7,19 +7,53 @@
 //
 
 import UIKit
+import MapKit
+
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var menuView: UIView!
+    @IBOutlet weak var leadingConstraint: NSLayoutConstraint!
+    var menuShowing = false
+    let regionRadius: CLLocationDistance = 800
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let initialLocation  = CLLocation(latitude:37.335187, longitude: -121.881072)
+        menuView.layer.shadowOpacity = 1
+        menuView.layer.shadowRadius = 6
+        centerMapOnLocation(location: initialLocation)
+    }
+    @IBAction func openMenu(_ sender: Any) {
+        if(menuShowing){
+            leadingConstraint.constant = -150
+        }else{
+            leadingConstraint.constant = 0
+            
+            UIView.animate(withDuration: 0.3, animations: {
+                self.view.layoutIfNeeded()
+            })
+        }
+        
+        menuShowing = !menuShowing
+    }
+    func centerMapOnLocation(location: CLLocation) {
+      
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+                                                                  regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
 
 }
 
